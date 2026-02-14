b0cd45600a6fb25fe9528196f6ddbe5c83f888c4
Aleks Pashkevych <alekspashkevych@gmail.com>
2026-02-14 14:15:01 +0200

    need to check

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,32 @@
 'use strict';
+
+// const logo = document.querySelector('.logo');
+const div = document.createElement('div');
+
+const promise1 = new Promise((resolve, reject) => {
+  let isClicked = false;
+
+  setTimeout(() => {
+    document.addEventListener('click', (e) => {
+      isClicked = true;
+    });
+  }, 3 * 1000);
+
+  if (isClicked) {
+    resolve('First promise was resolved');
+  } else {
+    reject(new Error('First promise was rejected'));
+  }
+});
+
+promise1
+  .then((message) => {
+    div.textContent = message;
+    document.body.append(div);
+  })
+  .catch((message) => {
+    setTimeout(() => {
+      div.textContent = message;
+      document.body.append(div);
+    }, 3 * 1000);
+  });
