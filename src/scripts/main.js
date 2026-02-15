3039710618765c5cc9cd003c60a794362103f6e0
Aleks Pashkevych <alekspashkevych@gmail.com>
2026-02-15 18:05:09 +0200

    corrected some mistakes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,9 @@
 'use strict';
 
 const div = document.createElement('div');
-const divHandler = (message) => {
+const divHandler = (message, result) => {
   div.setAttribute('data-qa', 'notification');
+  div.classList.add(result);
   div.textContent = message;
   document.body.appendChild(div);
 };
@@ -12,7 +13,7 @@ const firstPromise = new Promise((resolve, reject) => {
     reject(new Error(`First promise was rejected`));
   }, 3 * 1000);
 
-  document.body.addEventListener('click', () => {
+  document.addEventListener('click', () => {
     resolve(`First promise was resolved`);
     clearTimeout(timeOut);
   });
@@ -20,7 +21,9 @@ const firstPromise = new Promise((resolve, reject) => {
 
 const secondPromise = new Promise((resolve, reject) => {
   document.addEventListener('mousedown', (e) => {
-    resolve(`Second promise was resolved`);
+    if (e.button === 0 || e.button === 2) {
+      resolve(`Second promise was resolved`);
+    }
   });
 });
 const thirdPromise = new Promise((resolve, reject) => {
@@ -44,24 +47,33 @@ const thirdPromise = new Promise((resolve, reject) => {
 
 firstPromise
   .then((message) => {
-    divHandler(message);
+    divHandler(message, 'success');
   })
   .catch((error) => {
-    divHandler(error);
+    divHandler(error, `error`);
+  })
+  .finally(() => {
+    removeEventListener();
   });
 
 secondPromise
   .then((message) => {
-    divHandler(message);
+    divHandler(message, 'success');
   })
   .catch((error) => {
-    divHandler(error);
+    divHandler(error, `error`);
+  })
+  .finally(() => {
+    removeEventListener();
   });
 
 thirdPromise
   .then((message) => {
-    divHandler(message);
+    divHandler(message, 'success');
   })
   .catch((error) => {
-    divHandler(error);
+    divHandler(error, `error`);
+  })
+  .finally(() => {
+    removeEventListener();
   });
