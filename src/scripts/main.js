7e38712da152c566200cb0dea7dcaa160e897548
Aleks Pashkevych <alekspashkevych@gmail.com>
2026-02-15 17:43:15 +0200

    task done with mistakws, need to check

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,32 +1,67 @@
 'use strict';
 
-// const logo = document.querySelector('.logo');
 const div = document.createElement('div');
+const divHandler = (message) => {
+  div.setAttribute('data-qa', 'notification');
+  div.textContent = message;
+  document.body.appendChild(div);
+};
 
-const promise1 = new Promise((resolve, reject) => {
-  let isClicked = false;
-
-  setTimeout(() => {
-    document.addEventListener('click', (e) => {
-      isClicked = true;
-    });
+const firstPromise = new Promise((resolve, reject) => {
+  const timeOut = setTimeout(() => {
+    reject(new Error(`First promise was rejected`));
   }, 3 * 1000);
 
-  if (isClicked) {
-    resolve('First promise was resolved');
-  } else {
-    reject(new Error('First promise was rejected'));
-  }
+  document.body.addEventListener('click', () => {
+    resolve(`First promise was resolved`);
+    clearTimeout(timeOut);
+  });
 });
 
-promise1
+const secondPromise = new Promise((resolve, reject) => {
+  document.addEventListener('mousedown', (e) => {
+    resolve(`Second promise was resolved`);
+  });
+});
+const thirdPromise = new Promise((resolve, reject) => {
+  let leftBtn = false;
+  let rightBtn = false;
+
+  document.addEventListener('mousedown', (e) => {
+    if (e.button === 0) {
+      leftBtn = true;
+    }
+
+    if (e.button === 2) {
+      rightBtn = true;
+    }
+
+    if (leftBtn && rightBtn) {
+      resolve('Third promise was resolved');
+    }
+  });
+});
+
+firstPromise
   .then((message) => {
-    div.textContent = message;
-    document.body.append(div);
+    divHandler(message);
   })
-  .catch((message) => {
-    setTimeout(() => {
-      div.textContent = message;
-      document.body.append(div);
-    }, 3 * 1000);
+  .catch((error) => {
+    divHandler(error);
+  });
+
+secondPromise
+  .then((message) => {
+    divHandler(message);
+  })
+  .catch((error) => {
+    divHandler(error);
+  });
+
+thirdPromise
+  .then((message) => {
+    divHandler(message);
+  })
+  .catch((error) => {
+    divHandler(error);
   });
